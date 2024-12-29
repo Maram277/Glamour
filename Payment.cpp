ba7e399a7690553166b05577a37f54d497f6576a
Maram277 <mramalshbli@gmail.com>
2024-12-29 15:13:41 +0100

    Update Payment.cpp

--- Payment.cpp
+++ Payment.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <string>
+#include "Payment.h"
+
 using namespace std;
+
+Payment::Payment(int paymentID, double amount, const string Date, const string Method)
+	:paymentID(paymentID), amount(amount), paymentDate(Date), paymentMethod(Method)
+{
+}
+
+int Payment::getPaymentID() const
+{
+	return this->paymentID;
+}
+
+double Payment::getAmount() const
+{
+	return amount;
+}
+
+string Payment::getPaymentMethod() const
+{
+	return paymentMethod;
+}
+
+void Payment::showPaymentInfo() const
+{
+	cout << "Payment information:" << endl;
+	cout << "¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨" << endl;
+	cout << "Payment ID: " << paymentID << endl;
+	cout << "Amount: " << amount << "SEK." << endl;
+	cout << "Paymnet date: " << paymentDate << endl;
+	cout << "Payment method: " << paymentMethod << endl;
+	cout << "¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨" << endl;
+}

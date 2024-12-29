2c3ac5a8fb93fd1f53a8fad386e1080f5c9e3505
Maram277 <mramalshbli@gmail.com>
2024-12-29 15:12:31 +0100

    Update booking.h

--- booking.h
+++ booking.h
@@ -1,3 +1,21 @@
 #pragma once
-#include <iostream>
+#include <string>
 
+using namespace std;
+
+class Booking
+{
+private:
+	int BookingID;
+	string BookingData;
+	string BookingTime;
+	int CustomerID;
+
+public:
+
+	Booking(int BooingID, const string data, const string time, int customer);
+
+	int getBookingId() const;
+	int getCustomerId() const;
+	void showBookingInfo() const;
+};

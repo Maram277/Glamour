d6e9326b2f20332a98ae3bcd9da55ac7b4f20c61
Maram277 <mramalshbli@gmail.com>
2024-12-29 15:13:22 +0100

    Update Payment.h

--- Payment.h
+++ Payment.h
@@ -1,1 +1,22 @@
+#pragma once
+#include <string>
+
 using namespace std;
+
+class Payment
+{
+private:
+	int paymentID;
+	double amount;
+	string paymentDate;
+	string paymentMethod;
+
+public:
+	Payment(int paymentID, double amount, const string paymentDate, const string paymentMethod);
+
+	int getPaymentID() const;
+	double getAmount() const;
+	string getPaymentMethod() const;
+	void showPaymentInfo() const;
+
+};
